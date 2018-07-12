e4474dae7dccedcebc213f32ee732a17fa7ebfe3
Hanh Nguyen Thi Hong <nthonghanh@tma.com.vn>
2018-07-12 16:21:17 +0700

    J2-6: React

--- src/containers/components/TableSimple.jsx
+++ src/containers/components/TableSimple.jsx
@@ -1,32 +1,32 @@
 import React from 'react';
 import ReactTable from "react-table";
 import Pagination from "react-js-pagination";
-// import PaginationSimple from "./PaginationSimple";
+import PropTypes from 'prop-types';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "react-table/react-table.css";
 import "./../../assets/css/app.css";
 
 const TableSimple = (props) => {
-	const {
-		makeData,
-		makeColumns,
+  const {
+    makeData,
+    makeColumns,
     activePage,
     totalData,
     handlePageChange,
     isPagination,
-	} = props;
-	return (
-		<div>
-			<ReactTable
-			noDataText="No result found"
+  } = props;
+  return (
+    <div>
+      <ReactTable
+        noDataText="No result found"
         filterable
-				data={ makeData }
-				columns={ makeColumns }
+        data={ makeData }
+        columns={ makeColumns }
         showPagination={ false }
-				className="-striped -highlight"
+        className="-striped -highlight"
         defaultFilterMethod={(filter, row) =>
           String(row[filter.id]) === filter.value }
-			/>
+      />
       <br />
       { isPagination && <Pagination
         activePage={ activePage }
@@ -38,8 +38,26 @@ const TableSimple = (props) => {
         itemClass="page-item"
         innerClass="pagination justify-content-center"
       /> }
-		</div>
-	);
+    </div>
+  );
+};
+
+TableSimple.propTypes = {
+  makeData: PropTypes.object,
+  makeColumns: PropTypes.array,
+  activePage: PropTypes.number,
+  totalData: PropTypes.number,
+  handlePageChange: PropTypes.func,
+  isPagination: PropTypes.bool,
+};
+
+TableSimple.defaultProps = {
+  makeData: null,
+  makeColumns: null,
+  activePage: null,
+  totalData: null,
+  handlePageChange: null,
+  isPagination: false,
 };
 
 export default TableSimple; 
