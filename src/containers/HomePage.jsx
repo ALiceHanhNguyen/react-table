8b15469e06ba696ca405fcc855d0e44aeb6d0918
Hanh Nguyen Thi Hong <nthonghanh@tma.com.vn>
2018-07-13 17:23:06 +0700

    J2-8: Add select, button, datetime

--- src/containers/HomePage.jsx
+++ src/containers/HomePage.jsx
@@ -1,30 +1,37 @@
 import React from "react";
 import { render } from "react-dom";
 import { Button } from 'react-bootstrap';
+import Select from 'react-select';
+import moment from 'moment';
 import TableSimple from './components/TableSimple';
+import { DATA, COUNTRY } from './../utils/data';
 import './../../node_modules/font-awesome/css/font-awesome.min.css';
 import "./../assets/css/app.css";
+import "./../assets/css/select.css";
 
 class HomePage extends React.Component {
   constructor() {
     super();
     this.handlePageChange = this.handlePageChange.bind(this);
+    this.updateValue = this.updateValue.bind(this);
+    this.updateDate = this.updateDate.bind(this);
     this.state = {
       activePage: 1,
+      selectValue: {},
     };
-    this.makeData = [{"id":1,"email":"trung@justa.io","created_at":"2017-02-02T03:12:06.000Z","updated_at":"2018-01-29T09:56:58.000Z","sale_id":5},{"id":2,"email":"takuma@justa.io","created_at":"2017-02-02T03:12:06.000Z","updated_at":"2018-01-29T09:26:32.000Z","sale_id":2},{"id":3,"email":"hakim@justa.io","created_at":"2017-02-02T03:12:06.000Z","updated_at":"2017-11-29T23:30:42.000Z","sale_id":1},{"id":4,"email":"casey@justa.io","created_at":"2017-02-02T03:12:06.000Z","updated_at":"2018-01-22T08:08:56.000Z","sale_id":3},{"id":5,"email":"victor@justa.io","created_at":"2017-02-02T03:12:06.000Z","updated_at":"2018-01-26T11:50:44.000Z","sale_id":4},{"id":6,"email":"romen@justa.io","created_at":"2017-02-02T03:12:06.000Z","updated_at":"2018-01-29T01:38:25.000Z","sale_id":7},{"id":7,"email":"info@justa.io","created_at":"2017-02-02T03:12:06.000Z","updated_at":"2017-08-09T23:47:04.000Z","sale_id":6},{"id":9,"email":"kelly@justa.io","created_at":"2017-09-04T00:34:06.000Z","updated_at":"2017-09-04T00:34:06.000Z","sale_id":9},{"id":10,"email":"mattias@justa.io","created_at":"2017-09-04T01:04:50.000Z","updated_at":"2017-09-27T11:31:10.000Z","sale_id":10},{"id":11,"email":"community@justa.io","created_at":"2017-09-07T06:46:24.000Z","updated_at":"2017-12-11T06:13:24.000Z","sale_id":11},{"id":12,"email":"tma@justa.io","created_at":"2017-10-04T08:43:57.000Z","updated_at":"2017-11-21T08:23:29.000Z","sale_id":12},{"id":13,"email":"juri@justa.io","created_at":"2017-10-16T04:19:09.000Z","updated_at":"2018-01-30T00:43:10.000Z","sale_id":13},{"id":14,"email":"anju@wahlandcase.com","created_at":"2017-10-19T05:44:55.000Z","updated_at":"2018-01-29T04:25:25.000Z","sale_id":14},{"id":15,"email":"emily@wahlandcase.com","created_at":"2017-12-11T02:22:35.000Z","updated_at":"2018-01-05T00:41:50.000Z","sale_id":15},{"id":16,"email":"nan@justa.io","created_at":null,"updated_at":null,"sale_id":16},{"id":17,"email":"amruta@justa.io","created_at":null,"updated_at":null,"sale_id":17},{"id":18,"email":"kotone@justa.io","created_at":null,"updated_at":null,"sale_id":18},{"id":19,"email":"opportunities@justa.io","created_at":null,"updated_at":null,"sale_id":19},{"id":20,"email":"makoto@justa.io","created_at":null,"updated_at":null,"sale_id":20}];
+    this.makeData = DATA;
     this.makeColumns = [
       {
         Header: "ID",
         accessor: "id",
-        Cell: row => ( <div className="pd-7-5" >{ row.value }</div> ),
+        Cell: row => ( <div className="pd-7-5 style-col" >{ row.value }</div> ),
         filterMethod: (filter, row) =>
                   String(row[filter.id]).startsWith(filter.value),
       },
       {
         Header: "Email",
         accessor: "email",
-        Cell: row => ( <div className="pd-7-5" >{ row.value }</div> ),
+        Cell: row => ( <div className="pd-7-5 style-col" >{ row.value }</div> ),
         filterMethod: (filter, row) =>
                   row[filter.id].startsWith(filter.value),
         sortMethod: (a, b) => {
@@ -39,12 +46,12 @@ class HomePage extends React.Component {
         accessor: "created_at",
         filterable: false,
         Cell: row => (
-          <div className="pd-7-5" >
+          <div className="pd-7-5 style-col" >
             <span>{ row.value }</span>
-            <Button bsStyle='success' className="float-right mr-5-7" onClick={ () => console.log('***111****') }>
+            <Button bsStyle='success' className="float-right mr-5-7" onClick={ () => console.log('1') }>
               <i className="fa fa-spinner fa-spin" />
             </Button>
-            <Button bsStyle='danger' className="float-right mr-5-7" onClick={ () => console.log('*******') }>
+            <Button bsStyle='danger' className="float-right mr-5-7" onClick={ () => console.log('2') }>
               <i className="fa fa-spinner fa-spin" />
             </Button>
           </div> ),
@@ -52,15 +59,37 @@ class HomePage extends React.Component {
 	    {
 	      Header: "Updated at",
         accessor: "updated_at",
-        Cell: row => ( <div className="pd-7-5" >{ row.value }</div> ),
+        Cell: row => ( <div className="pd-7-5 style-col" >{ row.value }</div> ),
         filterMethod: (filter, row) =>
                   String(row[filter.id]).startsWith(filter.value),
 	    },
 	    {
+	      Header: "Country",
+        Cell: ({ original }) => (
+  				<Select
+						options={COUNTRY}
+						value={this.state.selectValue[original.id]}
+						onChange={(e) => this.updateValue(e, original)}
+						simpleValue
+					/> ),
+	    },
+      {
+        Header: "Date",
+        Cell: ({ original }) => (
+          <div className="style-col"><input
+            type="date"
+            name="bday"
+            className="date"
+            defaultValue={ moment(new Date()).format('YYYY-MM-DD') }
+            onChange={(e) => this.updateDate(e.target.value, original)}
+          /></div>
+        ),
+      },
+	    {
 	      Header: "ID Sale",
         accessor: "sale_id",
 	      Cell: row => (
-          <div className="style-col" style={{ backgroundColor: row.value > 10 ? '#85cc00' : row.value > 5 ? '#ffbf00' : '#ff2e00' }}>
+          <div className="style-col color-white" style={{ backgroundColor: row.value > 10 ? '#85cc00' : row.value > 5 ? '#ffbf00' : '#ff2e00' }}>
             { row.value }
           </div>
         ),
@@ -89,6 +118,18 @@ class HomePage extends React.Component {
 
   handlePageChange(pageNumber) {
     this.setState({activePage: pageNumber});
+  }
+
+  updateValue(newValue, original) {
+    const { selectValue } = this.state;
+    selectValue[original.id] = newValue;
+	  this.setState({ selectValue });
+  }
+
+  updateDate(newValue, original) {
+    const { dateValue } = this.state;
+    console.log('*12', newValue);
+    console.log('*13', original.id);
   }
 
   render() {
