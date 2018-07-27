6937c5a284e0f459f145cb01b3fba3f23103f407
nthonghanh <nthonghanh@tma.com.vn>
2018-07-27 23:41:13 +0700

    J2-11: reactstrap

--- src/Router.js
+++ src/Router.js
@@ -4,12 +4,14 @@ import { Switch, Route } from "react-router-dom";
 import HomePage from "./containers/HomePage";
 import LoginPage from "./containers/LoginPage";
 import SelectPage from "./containers/SelectPage";
+import RankingToolPage from "./containers/RankingToolPage";
 
 const Router = () => (
   <Switch>
     <Route exact path="/" component={ HomePage } />
     <Route exact path="/login" component={ LoginPage } />
     <Route exact path="/select" component={ SelectPage } />
+    <Route exact path="/tool" component={ RankingToolPage } />
   </Switch>
 );
 
